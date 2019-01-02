6bc2d5b6e2b520564224fd591dc1718d83a3956b
Pedro Bibiloni <pedro@bibiloni.es>
2019-01-02 12:10:05 +0100

    Added results and print management

--- core.py
+++ core.py
@@ -1,8 +1,13 @@
+import math
+
 import numpy as np
+from skimage import img_as_float
 from softcolor import MorphologyInCIELab
 
 
 def remove_and_inpaint(image, tophats_se=None, inpainting_se=None):
+
+    image = img_as_float(image)
 
     if tophats_se is None:
         tophats_se = [se_bar(side_enclosing_square_in_px=9, orientation_in_degrees=a)
@@ -13,16 +18,31 @@ def remove_and_inpaint(image, tophats_se=None, inpainting_se=None):
     morphology = MorphologyInCIELab()
     tophats_as_list = [morphology.tophat_closing(image, structuring_element=se)
                        for se in tophats_se]
-    tophats = np.concatenate(tophats_as_list, axis=3)
+    tophats = np.stack(tophats_as_list, axis=2)     # Since THs are monochannel (2D), so are to be stacked on 3rd.
 
-    curvilinear_detector = np.max(tophats, axis=3) - np.min(tophats, axis=3)
+    curvilinear_detector = np.max(tophats, axis=2) - np.min(tophats, axis=2)
     curvilinear_mask = curvilinear_detector > 0.1
 
-    inpaint, inpaint_steps = morphology.inpaint_with_steps(image, structuring_element=inpainting_se)
+    inpainted_image, inpaint_steps = morphology.inpaint_with_steps(image, structuring_element=inpainting_se)
 
-    steps = [v for tupl in zip(tophats_se, tophats_as_list) for v in tupl] + [curvilinear_detector, curvilinear_mask] + list(inpaint_steps)
+    steps = [v for tupl in zip(tophats_se, tophats_as_list) for v in tupl] + \
+            [curvilinear_detector, curvilinear_mask] + \
+            list(inpaint_steps)
 
-    return inpaint, steps
+    return inpainted_image, steps
+
 
 def se_bar(side_enclosing_square_in_px, orientation_in_degrees):
-    return None
+    se_sz = side_enclosing_square_in_px
+    sz_ct = side_enclosing_square_in_px // 2
+    m = -math.tan(math.radians(orientation_in_degrees))
+    [coord_x, coord_y] = np.meshgrid(range(-sz_ct, se_sz-sz_ct), range(-sz_ct, se_sz-sz_ct))
+
+    if m > 1e15:
+        distance_to_line = np.abs(coord_x)
+    else:
+        distance_to_line = np.abs(m * coord_x - coord_y) / math.sqrt(m ** 2 + 1)
+
+    variance = max(1/2, se_sz/14)
+    structuring_element = np.exp(-distance_to_line**2 / (2*variance))
+    return structuring_element

57a96b6d02b88c532c002802eeb4df13ac07d8cc
PBibiloni <pedro@bibiloni.es>
2019-01-11 17:48:47 +0100

    Visualize missing pixels as white

--- examples/utils.py
+++ examples/utils.py
@@ -14,6 +14,17 @@ def plot(list_images, show=True, save=False, save_each_independently=False, fold
     if isinstance(list_images, np.ndarray):
         list_images = [list_images]
 
+    for img in list_images:
+        # Show bool images
+        if img.dtype == 'bool':
+            img = img_as_uint(img)
+        # Visualize missing values as 1's (RGB's white)
+        mask_nans = np.isnan(img)
+        if np.any(mask_nans):
+            img = img.copy()
+            img[mask_nans] = 1
+
+
     if len(list_images) > 9:
         for i in range(math.ceil(len(list_images)/9)):
             plot(list_images[9*i:9*(i+1)], show=show, save=save, save_each_independently=save_each_independently,
@@ -33,14 +44,6 @@ def plot(list_images, show=True, save=False, save_each_independently=False, fold
         fig, axs = plt.subplots(nrows=sz[0], ncols=sz[1])
         [ax.axis('off') for ax in axs.flat]
         for idx, img in enumerate(list_images):
-            # Show bool images
-            if img.dtype == 'bool':
-                img = img_as_uint(img)
-            # Visualize missing values as 1's (RGB's white)
-            mask_nans = np.nan(img)
-            if np.any(mask_nans):
-                img = img.copy()
-                img[mask_nans] = 1
             ax = axs.flat[idx]
             im = ax.imshow(img)
             if img.ndim == 2:
@@ -58,8 +61,6 @@ def plot(list_images, show=True, save=False, save_each_independently=False, fold
                 for idx, single_image in enumerate(list_images):
                     os.makedirs(f'{folder}/{name}', exist_ok=True)
                     try:
-                        if single_image.dtype == 'bool':
-                            single_image = img_as_uint(single_image)
                         io.imsave(arr=single_image, fname=f'{folder}/{name}/{name} - {idx:02d}{extension}')
                     except ValueError as e:
                         logger.warning(f'Could not save image. Error: {e}')
